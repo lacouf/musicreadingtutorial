b63850d35fc8f707b9232069731bf4607be29aa1
Francois Lacoursiere <lacouf@gmail.com>
2025-11-21 12:40:52 -0500

    note log autoscroll

--- src/components/LogDisplay.jsx
+++ src/components/LogDisplay.jsx
@@ -1,11 +1,19 @@
 // src/components/LogDisplay.jsx
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 export default function LogDisplay({ log }) {
+    const logContainerRef = useRef(null);
+
+    useEffect(() => {
+        if (logContainerRef.current) {
+            logContainerRef.current.scrollTop = logContainerRef.current.scrollHeight;
+        }
+    }, [log]);
+
     return (
         <div style={{ marginTop: 10 }}>
             <h4>Log</h4>
-            <div style={{ maxHeight: 200, overflow: 'auto', background: '#fafafa', padding: 8 }}>
+            <div ref={logContainerRef} style={{ maxHeight: 200, overflow: 'auto', background: '#fafafa', padding: 8 }}>
                 {log.map((l, i) => <div key={i}><code>{l}</code></div>)}
             </div>
         </div>
